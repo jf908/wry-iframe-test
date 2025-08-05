9b1eb0c5bd8a289a2cd72a828e2684f071082363
jf908 <josh@thefindons.com>
2025-08-05 14:14:20 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use tao::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use wry::WebViewBuilder;
+
+fn main() -> wry::Result<()> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let builder = WebViewBuilder::new()
+        .with_url("https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/iframe")
+        .with_navigation_handler(|url| {
+            println!("Navigation handler to {}", url);
+            true
+        })
+        .with_new_window_req_handler(|url| {
+            println!("New window request handler to {}", url);
+            true
+        });
+
+    #[cfg(any(
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "android"
+    ))]
+    let _webview = builder.build(&window)?;
+    #[cfg(not(any(
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "android"
+    )))]
+    let _webview = {
+        use tao::platform::unix::WindowExtUnix;
+        use wry::WebViewBuilderExtUnix;
+        let vbox = window.default_vbox().unwrap();
+        builder.build_gtk(vbox)?
+    };
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } = event
+        {
+            *control_flow = ControlFlow::Exit;
+        }
+    });
+}
